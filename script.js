d266c04de3259fadb07c444feea2b139db5c38ea
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-09-26 11:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let circles = document.querySelectorAll('.circle')
+
+let next = document.getElementById('next')
+let perv = document.getElementById('perv')
+let count = 0;
+next.addEventListener('click',()=>{
+	if(count == 5){
+		return;
+	}
+	count++;
+	circles.forEach((cir)=>{
+		cir.classList.remove('active')
+	})
+	let circle = document.getElementById(`circle-${count}`)
+	circle.classList.add('active')
+})
+prev.addEventListener('click',()=>{
+	if(count == 1){
+		return;
+	}
+	count--;
+	circles.forEach((cir)=>{
+		cir.classList.remove('active')
+	})
+	let circle = document.getElementById(`circle-${count}`)
+	circle.classList.add('active')
+})
+
